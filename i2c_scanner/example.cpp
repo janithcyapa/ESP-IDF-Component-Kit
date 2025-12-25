c05b5236077b60baabd14746ec245a942f646528
Janith Yapa <janithcyapa@gmail.com>
2025-12-25 09:07:47 +0530

    Added vl53l0x

---
+++ i2c_scanner/example.cpp
@@ -0,0 +1,26 @@
+#include "i2c_scanner.hpp"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+
+extern "C" void app_main() { 
+    static const char* TAG = "main";
+
+    ESP_LOGI(TAG, "Starting application");
+
+    i2c_scanner::I2cScanner scanner({
+        .sda_io_num = GPIO_NUM_21,
+        .scl_io_num = GPIO_NUM_22,
+        .i2c_port = I2C_NUM_0,
+        .clk_speed = 100000
+    });
+
+    scanner.scan();
+
+    while (true) {
+        vTaskDelay(pdMS_TO_TICKS(10000));
+        scanner.scan();
+    }
+}
+
